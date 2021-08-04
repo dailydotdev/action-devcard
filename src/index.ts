0e1cc2cb8c8a2993c62e448fa65d64667e1115d0
Ole-Martin Bratteng <ole-martin@bratteng.com>
2021-08-04 23:23:28 +0200

    feat: create basic action to print out devcard url

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import core from '@actions/core'
+import github from '@actions/github'
+
+const devcardURL = (hash: string): string => `https://api.daily.dev/devcards/${hash}.svg`
+
+const run = async () => {
+	try {
+		const hash = core.getInput('hash')
+		core.debug(`Fetching devcard for hash: ${hash}`)
+		console.log(devcardURL(hash))
+
+		// Get github context data
+		const { repo } = github.context
+		console.log(`We can even get context data, like the repo: ${repo.repo}`)
+	} catch (error) {
+		core.setFailed(error.message)
+	}
+}
+
+run()
